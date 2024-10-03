d9effd55683bd5b9829df7f61e8917e2be934bbd
GMIKE <roman.soloweow333@ya.ru>
2024-10-03 21:26:24 +0100

    ref(Tests): FindParserExtensions, structural search tests

---
+++ src/SimpleStateMachine.StructuralSearch/Extensions/IFindParserExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace SimpleStateMachine.StructuralSearch.Extensions;
+
+public static class FindParserExtensions
+{
+    public static IEnumerable<FindParserResult> ParseString(this IFindParser findParser, string inputString)
+    {
+        var input = Input.String(inputString);
+        IParsingContext context = new ParsingContext(input);
+        return findParser.Parse(ref context);
+    }
+    
+    public static IEnumerable<FindParserResult> ParseFile(this IFindParser findParser, FileInfo fileInfo)
+    {
+        var input = Input.File(fileInfo);
+        IParsingContext context = new ParsingContext(input);
+        return findParser.Parse(ref context);
+    }
+}
